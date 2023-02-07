aba6266983c3a50cee609b858a0554961d19e5ea
Urmi Mahapatra <urmi.mahapatra@gmail.com>
2023-02-07 22:23:47 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,75 @@
 // import logo from './logo.svg';
+import React from 'react';
+import { useState } from 'react';
 import './App.css';
-import Task from'./Task/Task.js';
-import TodoItem from'./TodoItem/TodoItem.js';
+import Task from './Task/Task.js';
+import TodoItem from './TodoItem/TodoItem.js';
 
 
-function App() {
-  return (
-    <div className="App">
-      <div className='Container'>
-      <h1 className="Title">TODO APP</h1>
-        <Task></Task>
-        <TodoItem></TodoItem>
+class App extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      Items: [{
+        "userId": 1,
+        "id": 2,
+        "title": "ABCD",
+        "completed": false
+      },]
+    };
+    this.getToDoItemList();
+  }
+
+  handleAdd(element) {
+    const newList = this.state.Items.concat( {element });
+    this.state.Items = newList;
+  }
+
+  getToDoItemList() {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+    .then(res => res.json())
+    .then(itemlist => { 
+      if(itemlist) {
+        // this.Items = itemlist;
+        itemlist.forEach(element => {
+          // console.log("PKS element : " + element.userId );
+          // this.state.Items.concat(element);
+        });
+      } 
+    });
+
+    // console.log(this.state.Items);
+    // this.Items.map((item) => console.log("PKS 101 : " + item));
+  }
+
+  RenderItems() {
+    const listItems = this.items.map(
+        (element) => {
+            return (
+              <TodoItem Item={element} />
+            )
+        }
+    )
+    return (
+        <div>
+            {listItems}
+        </div>
+    )
+}
+
+  render() {
+    return (
+      <div className="App">
+        <div className='Container'>
+          <h1 className="Title">TODO APP</h1>
+          <Task />
+          {/* <this.RenderItems /> */}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+
 }
 
 export default App;
--- src/TodoItem/TodoItem.js
+++ src/TodoItem/TodoItem.js
@@ -5,18 +5,28 @@ import React from 'react';
 import './TodoItem.css';
 
 class TodoItem extends React.Component {
-    // constructor(props){
-    //     super(props);
-    //}
+    constructor(props){
+        super(props);
+        this.Item  = props.Item;
+        this.setChecked(this.Item.completed);
+
+    }
+    setChecked (isChecked){
+        if (isChecked){
+            this.isChecked = "Checked";
+        }else{
+            this.isChecked = "UnChecked";
+        }
+        
+    }
 
     render() {
         return (
             <div className='DisplayBox'>
-                <h1 className="displayTitle" ></h1>
-                <img src={Check} class='Check'></img>
-                <img src={Mark} class='Mark'></img>
-                <img src={Remove} class='Remove'></img>
-
+                <h1 className="displayTitle" >{this.Item.title}</h1>
+                <img src={Check} className='Check' target={this.isChecked}></img>
+                <img src={Mark} className='Mark'></img>
+                <img src={Remove} className='Remove'></img>
             </div>
         );
     }
