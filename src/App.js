26d51886b486bd50a892e2d7638a86c6e4311333
Urmi Mahapatra <urmi.mahapatra@gmail.com>
2023-02-07 22:26:24 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // import logo from './logo.svg';
 import React from 'react';
+import Async from 'react-async'
 import { useState } from 'react';
 import './App.css';
 import Task from './Task/Task.js';
@@ -7,64 +8,85 @@ import TodoItem from './TodoItem/TodoItem.js';
 
 
 class App extends React.Component {
-  
+
   constructor(props) {
     super(props);
-    this.state = {
-      Items: [{
-        "userId": 1,
-        "id": 2,
-        "title": "ABCD",
-        "completed": false
-      },]
-    };
-    this.getToDoItemList();
+    this.Items = [];
+    // this.state = {
+    //   Items: [{
+    //     "userId": 1,
+    //     "id": 2,
+    //     "title": "ABCD",
+    //     "completed": false
+    //   },]
+    // };
+    // this.getToDoItemList();
+    // console.log(this.Items);
+
   }
 
-  handleAdd(element) {
-    const newList = this.state.Items.concat( {element });
-    this.state.Items = newList;
-  }
+  // handleAdd(element) {
+  //   const newList = this.state.Items.concat( {element });
+  //   this.state.Items = newList;
+  // }
 
-  getToDoItemList() {
-    fetch("https://jsonplaceholder.typicode.com/todos")
-    .then(res => res.json())
-    .then(itemlist => { 
-      if(itemlist) {
-        // this.Items = itemlist;
-        itemlist.forEach(element => {
-          // console.log("PKS element : " + element.userId );
-          // this.state.Items.concat(element);
-        });
-      } 
-    });
+  async getToDoItemList() {
+    await fetch("https://jsonplaceholder.typicode.com/todos")
+      .then(res => res.json())
+      .then(itemlist => {
+        if (itemlist) {
+          this.Items = itemlist;
+          itemlist.forEach(element => {
+            // console.log("PKS element : " + element.userId );
+            // this.state.Items.concat(element);
+          });
+        }
+      });
 
     // console.log(this.state.Items);
     // this.Items.map((item) => console.log("PKS 101 : " + item));
   }
 
-  RenderItems() {
-    const listItems = this.items.map(
-        (element) => {
-            return (
-              <TodoItem Item={element} />
-            )
-        }
-    )
-    return (
-        <div>
-            {listItems}
-        </div>
-    )
-}
+    loadToDoItems = () =>
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then(res => (res.ok ? res : Promise.reject(res)))
+      .then(res => res.json());
+  //   RenderItems() {
+  //     const listItems = this.Items.map(
+  //         (element) => {
+  //             return (
+  //               <TodoItem Item={element} />
+  //             )
+  //         }
+  //     )
+  //     return (
+  //         <div>
+  //             {listItems}
+  //         </div>
+  //     )
+  // }
 
   render() {
+    this.loadToDoItems();
     return (
       <div className="App">
         <div className='Container'>
           <h1 className="Title">TODO APP</h1>
           <Task />
-          {/* <this.RenderItems /> */}
+          <Async promiseFn={this.loadToDoItems}>
+            {({ data, err, isLoading }) => {
+              if (isLoading) return "Loading..."
+              if (err) return `Something went wrong: ${err.message}`
+              if (data)
+                return (
+                  <div>
+                    {data.map(user => (
+                      <TodoItem Item={user} />
+                    ))}
+                  </div>
+                )
+            }}
+          </Async>
         </div>
       </div>
     );
