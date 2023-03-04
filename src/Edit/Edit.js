c2fbd279534b4e198348c223d338ff54242ee30b
Urmi Mahapatra <urmi.mahapatra@gmail.com>
2023-03-05 02:31:50 +0530

    commit1

--- src/Edit/Edit.js
+++
@@ -1,9 +0,0 @@
-
-// import './App.js';
-// import './TodoItem.js';
-// import './TodoItem.css';
-
-
-
-
-// <img src={Check} className='Check' target={this.isChecked} ></img>
--- src/data.js
+++
@@ -1,9 +0,0 @@
-// // import { useState } from "react";
-
-// // class DataInfo {
-// //     constructor (props) {
-// //         this.todoItems = props.Items;
-// //     }
-// // }
-
-// export let ToDoItemList = [];
